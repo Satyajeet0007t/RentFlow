22e53b83d9ba3438d75a0361d2577f1002571272
Satyajeet0007t <satyajit.nbnstic.comp@gmail.com>
2026-04-19 11:53:54 +0530

    fix: updated CORS policy for production

--- rentflow/server/index.js
+++ rentflow/server/index.js
@@ -16,7 +16,13 @@ const loginRoute = require("./routes/login");
 const app = express();
 
 // 2. --- MIDDLEWARE ---
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
